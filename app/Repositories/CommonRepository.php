c3923b7d6053ee5eb724f503d5c40d6a6544bcb4
chandraodgroup <76548819+chandraodgroup@users.noreply.github.com>
2021-10-25 18:58:56 +0530

    Social Media and Common Setting

--- app/Repositories/CommonRepository.php
+++ app/Repositories/CommonRepository.php
@@ -3,6 +3,9 @@
 namespace App\Repositories;
 use Illuminate\Http\Request;
 use App\Models\Banner;
+use App\Models\SocialMedia;
+use App\Models\OdbusCharges;
+
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Config;
@@ -11,15 +14,21 @@ use DateTime;
 class CommonRepository
 {
     protected $banner;
+    protected $socialMedia;
+    protected $odbusCharges;
 
-    public function __construct(Banner $banner)
+    public function __construct(Banner $banner, SocialMedia $socialMedia, OdbusCharges $odbusCharges)
     {
         $this->banner = $banner;
+        $this->socialMedia = $socialMedia;
+        $this->odbusCharges = $odbusCharges;
     }   
     
     public function getAll($request)
     { 
-        $data=$this->banner->where('bus_operator_id','=',$request['bus_operator_id'])->get();
+        $data['banner']=$this->banner->where('bus_operator_id','=',$request['bus_operator_id'])->get();
+        $data['socialMedia']=$this->socialMedia->where('bus_operator_id','=',$request['bus_operator_id'])->get();
+        $data['common']=$this->odbusCharges->where('bus_operator_id','=',$request['bus_operator_id'])->get();
         return $data;
     }
 
