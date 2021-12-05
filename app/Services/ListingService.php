75de0f4ce0219699c4b30324dd63bd2ed4948025
LIMA MOHANTY <chandraodgroup>
2021-12-05 15:34:31 +0530

    changes

--- app/Services/ListingService.php
+++ app/Services/ListingService.php
@@ -154,34 +154,47 @@ class ListingService
              $totalSeats = $record->busSeats->where('ticket_price_id',$ticketPriceId)->count('id');
                                           
              $seatDatas = $record->busSeats->where('ticket_price_id',$ticketPriceId)->all();
-             $amenityDatas = $record->busAmenities;  
-            if($amenityDatas)
+             $amenityDatas = [];  
+            if($record->busAmenities)
             {
-                foreach($amenityDatas as $a){
-                    if($a->amenities != null && isset($a->amenities->amenities_image) )
+                foreach($record->busAmenities as $a){
+                    if($a->amenities != NULL)
                     {
-                        $a->amenities->amenities_image = $path->amenity_url.$a->amenities->amenities_image;   
+                        if($a->amenities->amenities_image !=''){
+                            $a->amenities->amenities_image = $path->amenity_url.$a->amenities->amenities_image;   
+                  
+                        }
+                        $amenityDatas[] = $a;
                     }
+
                 }
             }
 
-             $safetyDatas = $record->busSafety;
-             if($safetyDatas)
+             $safetyDatas = [];
+             if($record->busSafety)
             {
-                foreach($safetyDatas as $sd){
-                    if($sd->safety != null && isset($sd->safety->safety_image) )
+                foreach($record->busSafety as $sd){
+                    if($sd->safety != NULL)
                     {
-                        $sd->safety->safety_image = $path->safety_url.$sd->safety->safety_image;   
+                        if($sd->safety->safety_image !=''){
+                            $sd->safety->safety_image = $path->safety_url.$sd->safety->safety_image; 
+                  
+                        }
+                        $safetyDatas[] = $sd;
+                         
                     }
                 }
             }
-             $busPhotoDatas = $record->busGallery;
-             if($busPhotoDatas)
+             $busPhotoDatas = [];
+             if($record->busGallery)
              {
-                 foreach($busPhotoDatas as $bp){
-                     if($bp->bus_image != null || isset($bp->bus_image) )
+                 foreach($record->busGallery as  $bp){
+                     if($bp->bus_image != null)
                      {
-                         $bp->bus_image = $path->busphoto_url.$bp->bus_image;   
+                        if($bp->bus_image!=''){
+                            $bp->bus_image = $path->busphoto_url.$bp->bus_image; 
+                        }
+                         $busPhotoDatas[] = $bp;
                      }
                  }
              }
