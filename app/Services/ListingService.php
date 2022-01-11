310a7997cc8320db977569863d5edda1b005b484
swagatika06 <swagatikasahu.seofied@gmail.com>
2022-01-11 12:51:48 +0530

    filter api issue fixed

--- app/Services/ListingService.php
+++ app/Services/ListingService.php
@@ -704,12 +704,9 @@ class ListingService
         }
         //return $FilterRecords;
         if($price == 0){
-            return $FilterRecords->sortBy([
-                ['departureTime', 'asc']
-            ]);
+            return $FilterRecords->sortBy(['departureTime', 'asc']);
         }elseif($price == 1){
-            $sortByPrice = $FilterRecords->sortBy('startingFromPrice')->all();
-            return $sortByPrice; 
+            return $FilterRecords->sortBy(['startingFromPrice', 'asc']);
        }  
        //$sorted = array_values($sorted);
       //return $sorted;
