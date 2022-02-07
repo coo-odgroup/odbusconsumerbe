fce952269dca522128aab6d9a1c5368729cc846b
LIMA MOHANTY <chandraodgroup>
2022-02-07 20:53:52 +0530

    by lima

--- app/Services/UsersService.php
+++ app/Services/UsersService.php
@@ -156,10 +156,6 @@ class UsersService
  
          if($list){
              foreach($list as $k => $l){
-
-
-                $l['users']->phone= (int) $l['users']->phone;
- 
                  $l['source']=$this->usersRepository->getLocation($l->source_id);
                  $l['destination']=$this->usersRepository->getLocation($l->destination_id);
                
