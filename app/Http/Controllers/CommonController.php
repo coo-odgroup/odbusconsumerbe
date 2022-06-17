21c484b7cdb6f1214b007fb9912ee0e2b534a869
swagatika06 <swagatikasahu.seofied@gmail.com>
2022-06-17 11:36:13 +0530

    app version api swagger added

--- app/Http/Controllers/CommonController.php
+++ app/Http/Controllers/CommonController.php
@@ -81,7 +81,27 @@ class CommonController extends Controller
            return $this->errorResponse($e->getMessage(),Response::HTTP_NOT_FOUND);
          }      
    } 
-
+/**
+     * @OA\Get(
+     *     path="/api/Appversion",
+     *     tags={"App Version"},
+     *     description="Get App Version",
+     *     summary="Get App Version",
+     *  @OA\Response(response="200", description="Get app version details"),
+     *  @OA\Response(response=206, description="validation error"),
+     *  @OA\Response(response=400, description="Bad request"),
+     *  @OA\Response(response=401, description="Unauthorized access"),
+     *  @OA\Response(response=404, description="No record found"),
+     *  @OA\Response(response=500, description="Internal server error"),
+     *  @OA\Response(response=502, description="Bad gateway"),
+     *  @OA\Response(response=503, description="Service unavailable"),
+     *  @OA\Response(response=504, description="Gateway timeout"),
+     *     security={
+     *       {"apiAuth": {}}
+     *     }
+     * )
+     * 
+     */
    public function Appversion(){
 
          $version = DB::table('app_version')->where("id",1)->get();
